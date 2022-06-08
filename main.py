5cbb2839aabaca94057569152ec9a5468524aade
pashkan111 <kozhevnikov0001@yandex.ru>
2022-06-08 16:50:48 +0300

    Added route for registration

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 import uvicorn
 from db import init_db
+from src.auth.routes import router
 
 
 app = FastAPI()
@@ -9,6 +10,8 @@ app = FastAPI()
 async def on_startup():
     await init_db()
 
+
+app.include_router(router)
 
 if __name__ == '__main__':
     uvicorn.run('main:app', reload=True, port=8000, host='0.0.0.0')
---
+++ src/auth/routes.py
@@ -0,0 +1,17 @@
+from fastapi import APIRouter, Depends, status
+from fastapi.responses import Response
+from sqlalchemy.ext.asyncio import AsyncSession
+from db import get_session
+from .managers import AuthManager
+from .auth_backend import check_auth_user
+from .models import AuthUser
+
+
+router = APIRouter()
+
+
+@router.post('/register')
+async def register(data: AuthUser, response: Response, session: AsyncSession = Depends(get_session)):
+    manager = AuthManager(session)
+    await manager.create_user(data)
+    response.status_code = status.HTTP_204_NO_CONTENT
