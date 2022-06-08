b0f7ee52f8f2d84af46fb4b681dc2abc3c0c7ea8
pashkan111 <kozhevnikov0001@yandex.ru>
2022-06-08 14:18:41 +0300

    Created manager for AuthUser

---
+++ src/auth/managers.py
@@ -0,0 +1,34 @@
+from typing import List
+from sqlalchemy.future import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from .models import AuthUser, Token
+from .services import get_password_hash, create_token
+
+
+class BaseManager:
+    """
+    Базовый менеджер моделей
+    """
+    def __init__(self, db_session: AsyncSession):
+        self.session = db_session
+
+
+class AuthManager(BaseManager):
+    async def create_user(self, login: str, password: str):
+        """
+        Создаем нового пользователя и токен
+        """
+        async with self.session.begin():
+            hashed_password = get_password_hash(password)
+            new_user = AuthUser(
+                login=login, password=hashed_password
+            )
+            self.session.add(new_user)
+            await self.session.commit()
+            
+            user_token = create_token()
+            new_token = Token(
+                user_token=user_token, user_id=new_user.id
+            )
+            self.session.add(new_token)
+            await self.session.commit()
